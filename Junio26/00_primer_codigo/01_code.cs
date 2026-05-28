97d5d4023e06435791707b1dda3ca440ded4f4d1
Nazareno <rubilnazareno@gmail.com>
2026-05-28 17:46:49 -0300

    .

---
+++ Junio26/00_primer_codigo/01_code.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("Hola mundo") ;
+//Console.WriteLine("Hello World!") ;
+
+Console.WriteLine("Congratulations!") ;
+Console.WriteLine(" ") ;
+Console.WriteLine(" You wrote your first lines of code.") ;
---
+++ Junio26/00_primer_codigo/02_write_writeline.cs
@@ -0,0 +1,4 @@
+Console.WriteLine("Congratulations") ;
+Console.Write("You wrote your first lines of code") ;
+// en resumen, WriteLine sgrega automaticamente el salto de linea al final de contenido. Osea que lo siguiente que escriba aparecera en la linea siguiente 
+// Write solo imprime el contenido pero sin salto de linea
