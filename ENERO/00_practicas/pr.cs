ed39b4221ff72d529e365a7f4f86b856f14b877a
Nazareno <narubil.it@gmail.com>
2026-05-30 05:37:16 -0300

    PRACTICAS

---
+++ ENERO/00_practicas/pr.cs
@@ -0,0 +1,57 @@
+BucleJuego() ;
+void BucleJuego()
+{
+    int vidaJugador = 30;
+    int vidaEnemigo = 30 ;
+    int ataqueJugador = 10 ;
+    int ataqueEnemigo = 10 ;
+    int accion = 0 ;
+    int experiencia =0 ;
+    int pociones = 3 ;
+   
+   
+    while (vidaJugador > 0 && vidaEnemigo > 0 )
+    {
+        Console.WriteLine("Es tu turno!\n1-Atacar\n2-Curarse");
+        accion = int.Parse(Console.ReadLine()) ;
+        
+           if (accion == 1)
+           {
+            vidaEnemigo = vidaEnemigo - ataqueJugador ;
+             Console.WriteLine($"Daño realizado!\nVida actual del enemigo:   {vidaEnemigo}\n") ;
+           }
+           if (vidaJugador > 30)
+              {
+            vidaJugador = 30 ;
+              }
+
+           if (accion == 2 && pociones > 0 )
+             {
+            vidaJugador + 15 ;
+            pociones = pociones - 1 ;
+            Console.WriteLine($"Te curaste 15 puntos de vida! vida actual: {vidaJugador}") ;
+             }
+            if (pociones == 0)
+        {
+            Console.WriteLine($"No te quedan mas pociones!");
+        } 
+
+           if (vidaEnemigo > 0)
+            {
+             vidaJugador = vidaJugador - ataqueEnemigo ;
+             Console.WriteLine ($"El enemigo ataca! DMG recibido: {ataqueEnemigo}\n----Tu vida actual: {vidaJugador}\n") ;
+             experiencia = experiencia + 50 ;
+            }
+            
+    }      
+     if (vidaEnemigo <= 0)
+            {
+            Console.WriteLine("Felicidades! derrotaste al enemigo") ;
+            }
+              else if (vidaJugador <= 0)
+            {
+            Console.WriteLine("Fuiste derrotado") ;
+            }
+             
+  
+}
