3dfadf85d254c791edb9450ad23abac30f892c56
Nazareno <narubil.it@gmail.com>
2026-05-30 21:09:28 -0300

    .

---
+++ Junio26/02_cadenas/02_concatenar.cs
@@ -0,0 +1,6 @@
+string firstName = "bob";
+string greeting = "Hello" ;
+string massage = greeting + " " + firstName + "!" ;
+Console.WriteLine(massage) ;
+
+// La mejor manera de hacer esto tambien es interpolar las cadenas de la siguiente manera
